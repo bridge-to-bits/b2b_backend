fae984ed39a8921d7a4dbe9dd41a3ff22cc6c60d
Dmytro Kyrychenko <dimakirimail@gmail.com>
2024-10-14 23:26:01 +0300

    enhancement #1: Configured Ocelot
    - configured ocelot package in gateway
    - implemented test redirection to wikipedia page
    
    closes #1

--- ApiGateway/Program.cs
+++ ApiGateway/Program.cs
@@ -1,28 +1,27 @@
 
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+
 namespace ApiGateway
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
 
+            builder.Configuration.AddJsonFile("ocelot.json", optional: false, reloadOnChange: true);
+            builder.Services.AddOcelot(builder.Configuration);
+
             builder.Services.AddControllers();
-            builder.Services.AddEndpointsApiExplorer();
-            builder.Services.AddSwaggerGen();
 
             var app = builder.Build();
 
-            if (app.Environment.IsDevelopment())
-            {
-                app.UseSwagger();
-                app.UseSwaggerUI();
-            }
-
             app.UseAuthorization();
 
-
             app.MapControllers();
+
+            await app.UseOcelot();
 
             app.Run();
         }
