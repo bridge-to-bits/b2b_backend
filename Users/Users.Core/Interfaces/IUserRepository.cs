28219ad11a55721f524697da47b79f377e5ebea3
Dmytro Kyrychenko <dimakirimail@gmail.com>
2024-10-18 21:17:40 +0300

    enhancement #6: Add userRepository Get and GetAll methods
    - implented Get and GetAll repository endpoints
    
    closes #6

--- Users/Users.Core/Interfaces/IUserRepository.cs
+++ Users/Users.Core/Interfaces/IUserRepository.cs
@@ -1,9 +1,13 @@
-﻿using Users.Core.Models;
+﻿using System.Linq.Expressions;
+using Users.Core.Models;
 
 namespace Users.Core.Interfaces
 {
     public interface IUserRepository
     {
         public Task<User> CreateUser(User user);
+
+        public Task<User> GetUser(Expression<Func<User, bool>> predicate);
+        public Task<IEnumerable<User>> GetUsers(Expression<Func<User, bool>> predicate);
     }
 }
