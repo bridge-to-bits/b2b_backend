24276a86fe8410622be175727a7761a02ffe341d
Dmytro Kyrychenko <dimakirimail@gmail.com>
2024-10-19 15:10:46 +0300

    refactor #9: used primary constructors
    - changed all constructors to primary constructor
    
    closes #9

--- Users/Users.Core/Services/PasswordHasher.cs
+++ Users/Users.Core/Services/PasswordHasher.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Users.Core.Interfaces;
+﻿using Users.Core.Interfaces;
 
 namespace Users.Core.Services
 {
