3a1ada6cc1d804776820653b1473cf66e5577e57
Dmytro Kyrychenko <dimakirimail@gmail.com>
2024-10-19 20:01:49 +0300

    enhancement #8: Implemented permissions logic
    - created AuthorizePermissionAttribute
    - added Include statements to repository get method to get user role and permissions
    - added permission check and add method to AuthService
    - improved registration method by adding role and permission for user
    - created setPermissions endpoint
    
    closes #8

--- Users/Users.Core/Interfaces/IUserRepository.cs
+++ Users/Users.Core/Interfaces/IUserRepository.cs
@@ -5,9 +5,11 @@ namespace Users.Core.Interfaces
 {
     public interface IUserRepository
     {
+        public Task CreateGrants(IEnumerable<Grant> grants);
+        public Task CreateRole(Role role);
         public Task<User> CreateUser(User user);
 
-        public Task<User> GetUser(Expression<Func<User, bool>> predicate);
+        public Task<User?> GetUser(Expression<Func<User, bool>> predicate);
         public Task<IEnumerable<User>> GetUsers(Expression<Func<User, bool>> predicate);
     }
 }
