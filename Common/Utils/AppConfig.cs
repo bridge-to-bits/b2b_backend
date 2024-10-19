6a4cbf50b01f8ace6bf8bbcaf6c7f37f50cbf68a
Dmytro Kyrychenko <dimakirimail@gmail.com>
2024-10-19 14:30:07 +0300

    Moved configuration setup to Common
    - implemented AppConfig class with necessary methods to access config

---
+++ Common/Utils/AppConfig.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace Common.Utils
+{
+    public static class AppConfig
+    {
+        private static readonly Lazy<IConfigurationRoot> _configuration = new(() =>
+        {
+            var baseDirectory = AppContext.BaseDirectory;
+            var solutionDirectory = Directory.GetParent(baseDirectory).Parent.Parent.Parent.Parent.Parent.FullName;
+
+            return new ConfigurationBuilder()
+                .SetBasePath(solutionDirectory)
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .Build();
+        });
+
+        public static IConfigurationRoot Configuration => _configuration.Value;
+
+        public static string GetConnectionString(string name) => Configuration.GetConnectionString(name);
+
+        public static string GetSetting(string key) => Configuration[key];
+
+        public static IConfigurationSection GetSection(string sectionName) => Configuration.GetSection(sectionName);
+    }
+}
