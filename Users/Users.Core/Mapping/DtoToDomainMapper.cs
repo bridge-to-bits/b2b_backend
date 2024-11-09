9d5120930874af014c8461fb4788af5469b115af
Dmytro Kyrychenko <dimakirimail@gmail.com>
2024-11-09 21:41:53 +0200

    enhancement #14: added AboutMe pole
    - updated Dto, Response and Model classes to include AboutMe prop
    - added migration file
    - updated mapping methods
    
    closes #14

--- Users/Users.Core/Mapping/DtoToDomainMapper.cs
+++ Users/Users.Core/Mapping/DtoToDomainMapper.cs
@@ -14,6 +14,7 @@ public static class DtoToDomainMapper
             LastName = registrationDTO.LastName,
             City = registrationDTO.City,
             Avatar = registrationDTO.Avatar,
+            AboutMe = registrationDTO.AboutMe,
             CreatedAt = DateTime.Now,
             UpdatedAt = DateTime.Now,
         };
