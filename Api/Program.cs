a37da047d4038153f9d9f7d08f8190782b3f5246
Dmytro Kyrychenko <dimakirimail@gmail.com>
2024-12-14 17:51:01 +0200

    enhancement: added usage of env variables instead of appsettings,json

--- Api/Program.cs
+++ Api/Program.cs
@@ -15,6 +15,8 @@ public class Program
     {
         var builder = WebApplication.CreateBuilder(args);
 
+        builder.Configuration.AddEnvironmentVariables();
+
         ServicesConfig(builder.Services);
         AppConfig.GeneralAuthConfig(builder.Services);
         AppConfig.DocsConfig(builder.Services);
